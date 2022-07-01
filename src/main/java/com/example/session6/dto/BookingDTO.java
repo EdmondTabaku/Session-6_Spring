66e0c603b27f87739d3633ff334bcf898bd14f7c
Edmond <edmondtabaku20@gmail.com>
2022-07-02 01:40:15 +0200

    Switched to Spring Data JPA

---
+++ src/main/java/com/example/session6/dto/BookingDTO.java
@@ -0,0 +1,45 @@
+package com.example.session6.dto;
+
+
+import java.sql.Date;
+import java.util.Set;
+
+public class BookingDTO {
+
+    private String status;
+    private Date bookingDate;
+    private UserDTO user;
+    private Set<FlightDTO> flights;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public Set<FlightDTO> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(Set<FlightDTO> flights) {
+        this.flights = flights;
+    }
+}
---
+++ src/main/java/com/example/session6/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package com.example.session6.dto;
+
+public class UserDTO {
+
+    private String username;
+    private String role;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
