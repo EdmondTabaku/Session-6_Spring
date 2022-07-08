10c5402451551495ba026de98cbeb921d228eaf3
Edmond <edmondtabaku20@gmail.com>
2022-07-08 17:49:56 +0200

    Added API

---
+++ src/main/java/com/example/session6/dto/UserDetailDTO.java
@@ -0,0 +1,46 @@
+package com.example.session6.dto;
+
+public class UserDetailDTO {
+
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////// GETTERS AND SETTERS /////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
